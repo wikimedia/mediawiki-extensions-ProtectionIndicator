ac67ff1ec36ad3c8fd6bd3f4fd8b71f8d4a984dd
Sohom Datta <dattasohom1@gmail.com>
2019-11-20 21:07:37 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ProtectionIndicator
+ProtectionIndicator is a personal project of mine where I try and create an extension over the span of a week, that adds a protection icon to each and every protected page. As of now, it only adds a black OOUI lock icon with a title explaining why the lock icon is there. 
+
+Among the things that need to be done are
+* Add support for customisation of the icon using javascript. I'll probably try to make this into a MediaWiki space on-wiki file thingy so that it can be manipulated per the wishes of the community.
+* Check if adding support for FlaggedRevs is worth it.
+* Convert the title into a OOUI popup and add some helpful links to it.
---
+++ includes/ProtectionIndicatorHooks.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @file
+ */
+
+namespace MediaWiki\Extension\ProtectionIndicator;
+
+class ProtectionIndicatorHooks {
+	/**
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/BeforePageDisplay
+	 * @param \OutputPage $out
+	 * @param \Skin $skin
+	 */
+	public static function onBeforePageDisplay( \OutputPage $out, \Skin $skin ) {
+		global $wgRestrictionLevels;
+		$config = $out->getConfig();
+		$title = $skin->getRelevantTitle();
+		$restrictionTypes = $title->getRestrictionTypes();
+		$o = new ProtectionIndicatorHooks;
+		foreach ( $restrictionTypes as $action ) {
+			$r = $title->getRestrictions( $action );
+			$rExpiry = $title->getRestrictionExpiry( $action );
+			foreach ( $wgRestrictionLevels as $level ) {
+				if ( in_array( $level, $r ) ) {
+					$o->createIndicator( $out, $action, $level, $rExpiry );
+				}
+			}
+		}
+		$rCascade = $title->getCascadeProtectionSources( true );
+		$r = $rCascade[1];
+		if ( $rCascade[0] ) {
+			foreach ( $restrictionTypes as $action ) {
+				if ( array_key_exists( $action, $r ) ) {
+					$r = $r[$action];
+					foreach ( $wgRestrictionLevels as $level ) {
+						if ( in_array( $level, $r ) ) {
+							$o->createIndicator( $out, $action, $level, null, true );
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * A function to create a padlock icon which is then
+	 * @param \OutputPage $out Output page object to write to
+	 * @param string $action Action for which protection has been applied
+	 * @param string $level Userright required to perform action
+	 * @param string|null $rExpiry Expiry time in 14 character format
+	 * @param bool $cascading | true is protection cascading
+	 */
+	protected function createIndicator( \OutputPage $out, $action,
+		$level, $rExpiry = null, $cascading = false ) {
+		$out->enableOOUI();
+		$out->addModuleStyles( [ 'oojs-ui.styles.icons-moderation' ] );
+		$timestamp = wfTimestamp( TS_RFC2822, $rExpiry );
+		if ( $cascading ) {
+			if ( strlen( $timestamp ) ) {
+				$icon = new \OOUI\IconWidget( [
+					'icon' => 'lock',
+					'title' => wfMessage( 'protection-indicator-explanation-cascading',
+					 $level, $action, $timestamp ),
+					'infuse' => true
+				] );
+			} else {
+				$icon = new \OOUI\IconWidget( [
+					'icon' => 'lock',
+					'title' => wfMessage( 'protection-indicator-explanation-cascading-infinity',
+					 $level, $action ),
+					'infuse' => true
+				] );
+			}
+		} else {
+			if ( strlen( $timestamp ) ) {
+				$icon = new \OOUI\IconWidget( [
+					'icon' => 'lock',
+					'title' => wfMessage( 'protection-indicator-explanation-non-cascading',
+					 $level, $action, $timestamp ),
+					'infuse' => true
+				] );
+			} else {
+				$icon = new \OOUI\IconWidget( [
+					'icon' => 'lock',
+					'title' => wfMessage( 'protection-indicator-explanation-non-cascading-infinity',
+					 $level, $action ),
+					'infuse' => true
+				] );
+			}
+		}
+		$out->setIndicators( [ 'protection-' . ( ( $cascading ) ? 'cascading-'
+		 : '' ) . $action => $icon ] );
+	}
+}
