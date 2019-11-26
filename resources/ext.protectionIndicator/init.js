7a7ae81bc3f293f014573e2ec7490250804c70d2
Sohom Datta <sohom.datta@learner.manipal.edu>
2019-11-26 13:46:52 +0530

    Fix errors detected by CI
    Bug: T239171
    Change-Id: Iaddf08abac35b860a19be471aec050c32bd6649c

--- resources/ext.protectionIndicator/init.js
+++ resources/ext.protectionIndicator/init.js
@@ -2,14 +2,14 @@
 	// eslint-disable-next-line no-jquery/no-global-selector
 	var $protectionIndicators = $( '.protection-indicator-icon' );
 	$protectionIndicators.each( function () {
-		var protectionExplanation, icon = OO.ui.infuse( this ), htmlContent, htmlfooter, reasonhtml = '', api = new mw.Api( {
+		var protectionExplanation, icon = OO.ui.infuse( this ), $htmlContent, $htmlfooter, $reasonhtml = '', api = new mw.Api( {
 			'user-agent': 'ProtectionIndicator-extension'
 		} );
 		function showProtectionExplanation( e ) {
 			e.preventDefault();
 			if ( !protectionExplanation ) {
 				protectionExplanation = new OO.ui.PopupWidget( {
-					$content: $( '<div>' ).html( htmlContent ).append( reasonhtml, htmlfooter ),
+					$content: $( '<div>' ).html( $htmlContent ).append( $reasonhtml, $htmlfooter ),
 					padded: true,
 					anchor: true,
 					autoClose: true,
@@ -24,8 +24,8 @@
 				protectionExplanation.toggle( false );
 			}
 		}
-		htmlContent = $( '<div>' ).addClass( 'protection-indicator-help-text' ).html( icon.getLabel() );
-		htmlfooter = $( '<div>' ).addClass( 'protection-indicator-footer' )
+		$htmlContent = $( '<div>' ).addClass( 'protection-indicator-help-text' ).html( icon.getLabel() );
+		$htmlfooter = $( '<div>' ).addClass( 'protection-indicator-footer' )
 			.append(
 				$( '<a>' ).addClass( 'protection-indicator-log-link' )
 					.attr( 'href',
@@ -42,7 +42,7 @@
 				lelimit: 1
 			} ).done( function ( response ) {
 				if ( response.query.logevents[ 0 ] && response.query.logevents[ 0 ].comment ) {
-					reasonhtml = $( '<div>' )
+					$reasonhtml = $( '<div>' )
 						.addClass( 'protection-indicator-reason' )
 						.html( mw.message( 'protection-indicator-reason-wrapper',
 							response.query.logevents[ 0 ].user,
